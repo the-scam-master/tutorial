68b9065adfa2e245bed828d5d924fc94465362e3
DataScienceZone <136954034+the-scam-master@users.noreply.github.com>
2025-08-31 10:43:28 +0530

    Update useThemeColors.ts

--- hooks/useThemeColors.ts
+++ hooks/useThemeColors.ts
@@ -1,5 +1,5 @@
-import { useTheme } from '@/contexts/ThemeContext';
-import { lightColors, darkColors, Colors } from '@/constants/colors';
+import { useTheme } from '../contexts/ThemeContext';
+import { lightColors, darkColors, Colors } from '../constants/colors';
 
 export const useThemeColors = (): Colors => {
   const { theme } = useTheme();
